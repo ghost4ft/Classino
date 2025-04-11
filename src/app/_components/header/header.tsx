236da1137238c7ca7105928f77a62ab7a77e8dcc
Mohamad <ghost4ft@gmail.com>
2025-04-11 04:16:01 +0330

    fix bugs

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 import Image from "next/image";
 import { TopNavigation } from "./top-navigation";
-
 export const Header: React.FC = () => {
   return (
-    <header className="border-b-[.5px] dark:border-base-content/60 dark:border-opacity-5">
+    <header className="border-b dark:border-base-content dark:border-opacity-5">
       <div className="container flex items-center justify-between">
         <Image
-          src="/images/logo-light.svg"
-          alt="Logo"
+          src="/images/logo.svg"
           width={100}
-          height={100}
+          height={36}
+          alt="کلاسینو"
         />
         <TopNavigation />
-        <span className="mr-auto">User Authentication</span>
+        <div className="mr-auto">User Authentication</div>
       </div>
     </header>
   );
 };
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="">
-   سلام
-  </div>;
+  return (
+    <>
+      <section className="bg-hero-pattern bg-no-repeat bg-center xl:bg-left mt-5 xl:mt-20">
+        <div className="container flex flex-col-reverse items-center xl:flex-row">
+          <div className="flex-1 flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right">
+            <h3 className="text-xl xl:text-2xl dark:text-info">
+              خوش اومدی به کلاسینو
+            </h3>
+            <h1 className=" text-3xl lg:text-5xl xl:text-5xl font-black gradient">
+              مسیر صعود به قله‌های پیشرفت
+            </h1>
+            <p className="max-w-2xl  text-lg md:text-xl font-bold leading-8">
+              با سامانه آموزش آنلاین، آموزش خود را به صورت آنلاین و به سهولت
+              تماشا کنید
+            </p>
+            <Image
+              src="/images/frameworks.png"
+              className="grayscale mt-4 opacity-70 m-auto xl:m-0"
+              width={412}
+              height={39}
+              alt=""
+            />
+          </div>
+          <Image
+            src="/images/programmer-landing.svg"
+            alt=""
+            width={702}
+            height={521}
+          />
+        </div>
+      </section>
+    </>
+  );
 }
